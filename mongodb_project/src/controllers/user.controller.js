422a758a64c8d28ecb8bb2087a7f8b3f00980438
hocuspocus07 <150130202+hocuspocus07@users.noreply.github.com>
2024-10-12 16:13:33 +0530

    added refresh and access tokens

--- mongodb_project/src/controllers/user.controller.js
+++ mongodb_project/src/controllers/user.controller.js
@@ -80,5 +80,38 @@ const registerUser = asyncHandler(async (req, res) => {
     .status(201)
     .json(new ApiResponse(200, createdUser, "user registered successfully"));
 });
-
-export { registerUser };
+const generateAccessAndRefreshToken=async(userId)=>{
+  try{
+    const user=await User.findById(userId);
+    const accessToken=user.generateAccessToken();
+    const refreshToken=user.genrateRefreshToken();
+    user.refreshToken=refreshToken;
+    user.save({validateBeforeSave:false})
+    return {accessToken,refreshToken};
+  }catch(error){
+    throw new ApiError(500,"something went wrong whle generating access and refresh token");
+  }
+}
+const loginUser=asyncHandler(async (req,res)=>{
+  //req body -> data
+  //usernmae or email
+  //find user
+  //password check
+  //access and refresh token
+  //send cookie
+  const {email,username,password}=req.body;
+  if (!username||!email){
+    throw new ApiError(500,"username or email is required")
+  }
+  const user=await User.findOne({
+    $or:[{username},{email}]
+  })
+  if (!user){
+    throw new ApiError(404,"User not found.");
+  }
+  const isPasswordValid=await user.isPasswordCorrect(password)
+  if (!isPasswordValid){
+    throw new ApiError(401,"Password is incorrect.");
+  }
+})
+export { registerUser,loginUser };
